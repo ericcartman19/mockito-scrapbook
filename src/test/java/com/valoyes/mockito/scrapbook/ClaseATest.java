b05b66752bb807f361c13cc40e4e0b1006c34e17
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-05-04 21:54:53 +0200

    use assertSame

--- src/test/java/com/valoyes/mockito/scrapbook/ClaseATest.java
+++ src/test/java/com/valoyes/mockito/scrapbook/ClaseATest.java
@@ -1,10 +1,11 @@
 package com.valoyes.mockito.scrapbook;
 
+import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 public class ClaseATest {
@@ -13,6 +14,7 @@ public class ClaseATest {
 	ClaseB claseB;
 	private ClaseA claseA;
 	
+	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		claseA = new ClaseA(claseB);
@@ -24,6 +26,14 @@ public class ClaseATest {
 		// when
 		claseA.usesVoidMethod();
 		// then
+		verify(claseB).voidMethod();
+	}
+	
+	@Test
+	public void usesVoidMethod_ShouldCallVoidMethod_CheckValue() throws Exception {
+		// para testear void methods, no hace falta utilizar when de manera explicita
+		// assert
+		assertSame(1, claseA.usesVoidMethod());
 		verify(claseB).voidMethod();
 	}
 
